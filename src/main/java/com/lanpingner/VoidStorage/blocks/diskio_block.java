dc1d58d575adcfc990e8f7aa3eedf0400639b4a1
mgdev <berromaa@gmail.com>
2020-05-17 01:13:07 -0700

    0.0.1

--- src/main/java/com/lanpingner/VoidStorage/blocks/diskio_block.java
+++ src/main/java/com/lanpingner/VoidStorage/blocks/diskio_block.java
@@ -1,8 +1,15 @@
 package com.lanpingner.VoidStorage.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 public class diskio_block extends Block {
@@ -13,5 +20,11 @@ public class diskio_block extends Block {
                 .harvestLevel(1)
                 .harvestTool(ToolType.PICKAXE)
         );
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+
+        return ActionResultType.SUCCESS;
     }
 }
